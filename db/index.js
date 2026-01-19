47c41e9f5cfca5325ee7aefdf7c745bc619ca642
Rebeca Mustelier Trinchet <mustelierrebeca99@gmail.com>
2026-01-19 17:51:24 -0600

    ✅ Backend fa sql

--- db/index.js
+++ db/index.js
@@ -2,12 +2,14 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "clinic",
-  ssl: false // no usar SSL en local
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 async function initDB() {
